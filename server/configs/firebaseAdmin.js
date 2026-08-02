151cc943e5630dcfc66c08686c6a83b1a0da73f4
mohit <mohit.sh7473@gmail.com>
2026-08-03 03:40:23 +0530

    firebase

--- server/configs/firebaseAdmin.js
+++ server/configs/firebaseAdmin.js
@@ -2,14 +2,14 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 initializeApp({
-    credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(
-            /\\n/g,
-            "\n"
-        ),
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n"
+    ),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 export const adminAuth = getAuth();
