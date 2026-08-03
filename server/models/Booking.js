b0ca507db8494fb533083e237af773bbbc88a513
mohit <mohit.sh7473@gmail.com>
2026-08-03 14:45:36 +0530

    Add QR code support

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,13 +1,53 @@
 import mongoose from "mongoose";
 
-const bookingSchema = new mongoose.Schema({
-    user: {type: String, required: true, ref: 'User'},
-    show: {type: String, required: true, ref: 'Show'},
-    amount: {type: Number, required: true},
-    bookedSeats: {type: Array, required: true},
-    isPaid: {type: Boolean,  default:false},
-    paymentLink: {type: String},
-},{timestamps: true })
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+
+    show: {
+      type: String,
+      required: true,
+      ref: "Show",
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    bookedSeats: {
+      type: Array,
+      required: true,
+    },
+
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+
+    paymentLink: {
+      type: String,
+      default: "",
+    },
+
+    qrCode: {
+      type: String,
+      default: "",
+    },
+
+    isUsed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
