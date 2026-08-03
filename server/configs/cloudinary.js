b74b4451708d606b0d380eb47170e765425d5cb2
mohit <mohit.sh7473@gmail.com>
2026-08-03 22:45:18 +0530

    mail

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -5,5 +5,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
 
 export default cloudinary;
